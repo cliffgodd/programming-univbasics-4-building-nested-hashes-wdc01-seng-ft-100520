89f77d9b5cb96ebb3fb9770962a869ef6cfe1fd7
Justin Clifford <justoon7.jc@gmail.com>
2020-09-21 04:02:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
       :pieces => 4,
       :rent_in_dollars => {
       },
-      :names => {        
+      :names => {
     }
   }
 }
@@ -36,13 +36,13 @@ def monopoly_with_third_tier
       },
       :names => {
         :reading_railroad => {
-          
+
         },
         :pennsylvania_railroad => {
-          
+
         },
         :b_and_o_railroad => {
-          
+
         },
         :shortline_railroad => {
           
